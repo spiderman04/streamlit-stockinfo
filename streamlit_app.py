b73ffd3fe2a6dc9894e1c87e7235e8eaf765c1e6
Spiderman <spiderman04@users.noreply.github.com>
2024-11-07 14:24:04 -0500

    correct end data string

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ st.write('Hello world!')
 
 # get basic ticker information
 tickerData = yf.Ticker('NVDA')
-tickerDf = tickerData.history(period='6mo', start='2024-01-01', end=2024-11-07')
+tickerDf = tickerData.history(period='6mo', start='2024-01-01', end='2024-11-07')
 
 st.lin_chart(tickerDf.Open)
 st.line_chart(tickerDf.Close)
