6b1cf73c54c45419f93fb3c7c6f7863b423bdfe7
Spiderman <spiderman04@users.noreply.github.com>
2024-11-07 14:25:41 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,6 +8,6 @@ st.write('Hello world!')
 tickerData = yf.Ticker('NVDA')
 tickerDf = tickerData.history(period='6mo', start='2024-01-01', end='2024-11-07')
 
-st.lin_chart(tickerDf.Open)
+st.line_chart(tickerDf.Open)
 st.line_chart(tickerDf.Close)
 st.line_chart(tickerDf.Volume)
